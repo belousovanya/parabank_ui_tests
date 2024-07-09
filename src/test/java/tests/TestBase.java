0eb3bb5e1af513bcf444da2e14509b7c5de46f2d
Anna Belousova <belusovanna@gmail.com>
2024-07-10 00:11:16 +0500

    added TestBase

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,12 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void setUpTests() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://parabank.parasoft.com";
+    }
+}
