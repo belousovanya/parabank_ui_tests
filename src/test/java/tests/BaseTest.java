1f1ed00e30c83668ca49ba6ff84d8861c02dac81
Anna Belousova <belusovanna@gmail.com>
2025-02-01 11:11:04 +0400

    added allure

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,12 +1,26 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
     @BeforeAll
     static void setUpTests() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://parabank.parasoft.com";
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+                .includeSelenideSteps(true));
+    }
+
+    @BeforeEach
+    public void openHomePage() {
+        Selenide.open(Configuration.baseUrl);
     }
 }
